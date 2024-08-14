41a9c0e97ec24f89372c8e32935090c9bc497dfa
Kyrylo <103639388+KyryloPapazov@users.noreply.github.com>
2024-08-14 13:04:12 +0300

    Added Index and About pages

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $viewData = [];
+        $viewData = ["title" => "Home Page - Store With Honey"];
+        return view('home.index')->with("viewData", $viewData);
+    }
+
+    public function about()
+    {
+        $viewData = [];
+        $viewData["title"] = "About Us - Online Stor for Bazarnichki-Medovichki";
+        $viewData["subtitle"] = "About Us";
+        $viewData["description"] = "This is about page ... ";
+        $viewData["author"] = "Developed by: Kyrylo Papazov";
+        return view('home.about')->with("viewData", $viewData);
+    }
+}
